c3b456fb2b1466ac911588a3d552564dae50ec19
Wolfgang Schnerring <wosc@wosc.de>
2011-03-18 08:49:21 +0100

    Fix fixture loading so the tests run

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,14 @@ require File.expand_path(File.dirname(__FILE__) + '/../../../../test/test_helper
 
 # Ensure that we are using the temporary fixture path
 Engines::Testing.set_fixture_path
+
+
+class ActionMailer::TestCase
+  # original uses hardcoded path from RAILS_ROOT, which of course doesn't work
+  def read_fixture(action)
+    IO.readlines(File.join(
+      ActiveSupport::TestCase.fixture_path,
+      self.class.mailer_class.name.underscore,
+      action))
+  end
+end
